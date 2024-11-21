5f49a6471668ef552ee96c5b6223f64a50dc91f6
SheilandraZarawiba29 <Sheilandrazarawiba561@gmail.com>
2024-11-21 12:27:52 +0700

    Pertanyaan 3 Percobaan 4

--- PengunjungCafe28.java
+++ PengunjungCafe28.java
@@ -1,12 +1,15 @@
 public class PengunjungCafe28 {
-    public static void daftarPengunjung(String... namaPengunjung) {
-        System.out.println("Daftar Nama Pengunjung:");
-        for (String nama : namaPengunjung) {  
-            System.out.println("- " + nama);
+    public static void daftarPengunjung(String[] namaPengunjung, int[] umurPengunjung) {
+        System.out.println("Daftar Pengunjung dan Umur:");
+
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i] + ", Umur: " + umurPengunjung[i]);
         }
     }
 
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+        String[] nama = {"Ali", "Budi", "Citra"};
+        int[] umur = {25, 30, 28};
+        daftarPengunjung(nama, umur);
     }
 }
